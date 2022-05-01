2390cb952ab9f809b1677a293124bc7c712f644e
Bl1xvan <84408872+Bl1xvan@users.noreply.github.com>
2022-05-01 13:52:28 +0000

    Replaced input text with buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React, {useState, useRef} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Color from './components/Colors'
 import Name from './components/Text'
 
 function App() {
   const [color, setColor] = useState('');
-  const newColor = useRef();
 
-  function changeColor(){
-    const name = newColor.current.value;
-    if(name === '') return;
+  function changeColor(e){
+    const name = e.target.value;
     setColor(name);
-    newColor.current.value = null;
   }
   
   return (
     <main>
       <h1 style={{color: color}}>Hello World!</h1>
-      <input ref={newColor} type='text' />
-      <button onClick={changeColor}>Change Color!</button>
+      <button onClick={changeColor} value="pink">pink</button>
+      <button onClick={changeColor} value="yellow">yellow</button>
+      <button onClick={changeColor} value="blue">blue</button>
+      <button onClick={changeColor} value="green">green</button>
+      <button onClick={changeColor} value="brown">brown</button>
+      <button onClick={changeColor} value="purple">purple</button>
     </main>
   );
 
